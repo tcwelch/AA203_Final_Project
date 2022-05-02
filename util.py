1b3db21a7602ebf2d7e38a421de18e814bf50a61
tcwelch <43589658+tcwelch@users.noreply.github.com>
2022-05-01 18:29:14 -0700

    fixed many bigs

--- util.py
+++ util.py
@@ -20,8 +20,8 @@ def main():
     q = 0.05*x0
     r = np.array([[0.0667],\
                   [6]]) #2% of x0 for G and I
-    Q = np.diag(q)
-    R = np.diag(r)
+    Q = np.diag(np.squeeze(q))
+    R = np.diag(np.squeeze(r))
     dynamics = Dynamics(x0,params,f_x,Q,R,N)
     #estimate of state info
     mu0 = np.array([[4.],\
@@ -33,18 +33,18 @@ def main():
                     [0.450/60],\
                     [0.05/60],\
                     [500]])
-    S0 = np.eye(8)
-    q_hat = q #np.array([])
+    S0 = np.eye(9)
+    q_hat = 0.05*np.vstack((x0,params)) #np.array([])
     r_hat = r #np.array([])
-    Q_hat = np.diag(q_hat)
-    R_hat = np.diag(r_hat)
+    Q_hat = np.diag(np.squeeze(q_hat))
+    R_hat = np.diag(np.squeeze(r_hat))
     window_size = 10
-    adaptive = True
+    adaptive = False
     ekf = EKF(mu0,S0,f_x_and_params,Q_hat,R_hat,adaptive,window_size,N)
     #simulation loop
     for i in range(1,len(t)):
-        u = 0
-        x,measurement = dynamics.update_state(dT)
+        u = np.array([0.])
+        x,measurement = dynamics.update_state(u,dT)
         mu = ekf.update_estimate(u,measurement,dT)
 
     #plot of state and estimate of state dynamics
@@ -56,7 +56,7 @@ def main():
     plt.title('States and Estimates vs. Time')
     plt.legend()
 
-    plt.subplot(3,1,3)
+    plt.subplot(3,1,2)
     plt.plot(t,dynamics.x[1,:dynamics.itr])
     plt.plot(t,ekf.mu[1,:ekf.itr])
     plt.ylabel('Insulin Concentration in Blood []')
@@ -104,6 +104,7 @@ def main():
     plt.show()
 
 def f_x(x,u,params):
+    params = np.squeeze(params)
     A = np.array([[0., -params[0], params[1]],\
                   [params[2], -params[3], 0.],\
                   [0., 0., -params[4]]])
@@ -112,34 +113,34 @@ def f_x(x,u,params):
 
 def f_x_and_params(x,u,params):
     x_short_dot = f_x(x,u,params)
-    x_long_dot = np.zeros((8,1))
-    x_long_dot[:3] = x_short_dot
+    x_long_dot = np.zeros((9,1))
+    x_long_dot[:3] = np.reshape(x_short_dot,(np.shape(x_long_dot[:3])))
     return x_long_dot
 
 class Dynamics():
-    def __init__(self,x0,params,f,Q,R,N=500):
+    def __init__(self,x0,params,f,Q,R,N):
         dim1 = np.shape(Q)
         self.n = dim1[0]
         dim2 = np.shape(R)
         self.m = dim2[0]
 
         self.x = np.zeros((self.n,N)) #np array
-        self.x[:,0] = x0
+        self.x[:,0] = np.squeeze(x0)
         self.params = params
         self.measurement = np.zeros((self.m,N))
-        self.measurement[:,0] = np.array([[x0[0]],[x0[1]]])
+        self.measurement[:,0] = np.squeeze(np.array([x0[0],x0[1]]))
         self.Q = Q
         self.R = R
         self.f = f
         self.itr = 1
 
-    def update_state(self,dT):
+    def update_state(self,u,dT):
+        self.make_space()
         i = self.itr
-        self.make_space(self)
-        self.x[:,i] = self.x[:,i-1] + dT*self.f(self.x[:,i-1]) \
-            + np.random.normal(np.zeros((self.n,1)),self.Q)
-        self.measurement[:,i] = np.array([[self.x[0,i]],[self.x[1,i]]])\
-            + np.random.normal(np.zeros((self.m,1)),self.R)
+        self.x[:,i] = np.squeeze(self.x[:,i-1] + dT*self.f(self.x[:,i-1],u,self.params) \
+            + np.random.multivariate_normal(np.zeros((self.n,)),self.Q))
+        self.measurement[:,i] = np.squeeze(np.array([self.x[0,i],self.x[1,i]])\
+            + np.random.multivariate_normal(np.zeros((self.m,)),self.R))
         self.itr += 1  
         return self.x[:,i],self.measurement[:,i]
 
@@ -151,14 +152,14 @@ class Dynamics():
 
 
 class EKF():
-    def __init__(self,mu0,S0,f,Q,R,adaptive=True,window_size=10,N=500):
+    def __init__(self,mu0,S0,f,Q,R,adaptive,window_size,N):
         dim1 = np.shape(Q)
         self.n = dim1[0]
         dim2 = np.shape(R)
         self.m = dim2[0]
 
         self.mu = np.zeros((self.n,N))
-        self.mu[:,0] = mu0
+        self.mu[:,0] = np.squeeze(mu0)
         self.mu_pred = np.zeros((self.n,N)) #<-- need to initialize?
         self.S = S0
         self.f = f
@@ -167,7 +168,7 @@ class EKF():
         self.C[1,1] = 1.0
         self.Q = Q
         self.R = R
-        self.adaptive = True
+        self.adaptive = adaptive
         self.W = window_size
         self.d = np.zeros((self.m,N))
         self.itr = 1
@@ -177,17 +178,18 @@ class EKF():
         self.make_space()
         #linearize A
         A = np.array([[0.,-self.mu[3,i-1],self.mu[4,i-1],-self.mu[1,i-1],self.mu[2,i-1],0.,0.,0.,0.],\
-            [self.mu[5,i-1],-self.mu[6,i-1],0.,0.,0.,self.mu[0,i-1],-self.mu[1,i-1],0.,u],\
+            [self.mu[5,i-1],-self.mu[6,i-1],0.,0.,0.,self.mu[0,i-1],-self.mu[1,i-1],0.,u[0]],\
             [0.,0.,-self.mu[7,i-1],0.,0.,0.,0.,-self.mu[2,i-1],0.],\
-            [0.,0.,0.,0.,0.,0.,0.,0.],\
-            [0.,0.,0.,0.,0.,0.,0.,0.],\
-            [0.,0.,0.,0.,0.,0.,0.,0.],\
-            [0.,0.,0.,0.,0.,0.,0.,0.],\
-            [0.,0.,0.,0.,0.,0.,0.,0.]]) #<--dimensions on x given estimating param from u??
+            [0.,0.,0.,0.,0.,0.,0.,0.,0.],\
+            [0.,0.,0.,0.,0.,0.,0.,0.,0.],\
+            [0.,0.,0.,0.,0.,0.,0.,0.,0.],\
+            [0.,0.,0.,0.,0.,0.,0.,0.,0.],\
+            [0.,0.,0.,0.,0.,0.,0.,0.,0.],\
+            [0.,0.,0.,0.,0.,0.,0.,0.,0.]]) #<--dimensions on x given estimating param from u??
         #discretize A
-        A = np.eye(np.shape(A)) + dT*A
+        A = np.eye(np.shape(A)[0]) + dT*A
         #predict
-        self.mu_pred[:,i] = self.f(self.mu[:3,i],u,self.mu[3:])
+        self.mu_pred[:,i] = np.squeeze(self.f(self.mu[:3,i-1],u,self.mu[3:,i-1]))
         S_predict = A@self.S@A.T + self.Q
 
         #linearize C with mu_pred (already done because C is constant)
@@ -196,13 +198,14 @@ class EKF():
         #calculated predicted measurement
         pred_measurement = self.C@self.mu_pred[:,i]
         #update
-        self.mu[:,i] = self.mu_pred[:,i] + K@(measurement - pred_measurement)
-        self.S = (np.eye(np.shape(A)) + K@self.C)@S_predict
+        self.mu[:,i] = np.squeeze(self.mu_pred[:,i] \
+                       + K@(measurement - pred_measurement))
+        self.S = (np.eye(np.shape(A)[0]) + K@self.C)@S_predict
 
         #update Q if adaptive
         if self.adaptive:
-            self.d[:,i] = measurement - pred_measurement
-            if len(i) < self.window_size:
+            self.d[:,i] = np.squeeze(measurement - pred_measurement)
+            if i < self.W:
                 D = (1/self.W)*np.sum(self.d[:,:i]@self.d[:,:i].T)
                 delta_x = self.mu[:,:i] - self.mu_pred[:,:i]
                 L = np.divide(delta_x,self.d[:,:i])
